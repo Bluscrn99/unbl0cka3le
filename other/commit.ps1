6798ed242e9ebe011122ae9965563422640243ee
TheIdiot0 <beckettphillips14@gmail.com>
2025-05-20 17:00:12 -0400

    Updated README.md

---
+++ other/commit.ps1
@@ -0,0 +1,11 @@
+# Say what the script is doing
+echo "Auto-commit"
+
+# Prompt for a commit message
+$commitMessage = Read-Host "What have you completed?"
+
+# Stage all changes
+git add .
+
+# Commit with the entered message
+git commit -m "$commitMessage"
